e1a89f872d75d3634ce77eebef8d85118d0a3b41
g2342051 <g2342051@tcu.ac.jp>
2026-02-07 03:14:08 +0900

    iroiro

--- comingsoon/assets/main.js
+++ comingsoon/assets/main.js
@@ -7,22 +7,22 @@ gsap.registerPlugin(ScrollTrigger);
             scrollTrigger: {
                 trigger: ".horizontal-section",
                 start: "top top",
-                end: "+=3000",   // スクロール量（重さの調節）
+                end: "+=2000",   // スクロール量（重さの調節）
                 pin: true,       // 画面固定
-                scrub: 0.5,      // 操作への追従速度（0.5は比較的軽め）
+                scrub: 1,      // 操作への追従速度（0.5は比較的軽め）
                 anticipatePin: 1
             }
         });
 
         // 1. 最初：少し「遊び」を入れて、すぐには動かないようにする
-        tl.to({}, { duration: 3 });
+        tl.to({}, { duration: 2 });
 
         // 2. 移動：横に50vwスライド
         tl.to(wrapper, {
             x: "-50vw",
             ease: "none",      // 等速で動かして軽さを出す
-            duration: 8
+            //duration: 8
         });
 
         // 3. 最後：少し「遊び」を入れて、急に縦スクロールに戻るのを防ぐ
-        tl.to({}, { duration: 3 }); 
+        tl.to({}, { duration: 2 }); 
